95334a4dbbce283b29f8804a12998bd9b89ae38c
wotoss <wotoss10@gmail.com>
2022-01-02 13:45:29 -0500

    react-proj-API-hooks

--- src/App.js
+++ src/App.js
@@ -1,23 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect, useMemo, useCallback } from 'react';
 
 function App() {
+
+  const [tarefas, setTarefas] = useState([]);
+  const [input, setInput] = useState('');
+
+
+  useEffect(()=>{
+    const tarefaStorage = localStorage.getItem('tarefas');
+      //neste if eu vejo se tem algo ou valor, tendo eu modifico ou atualizo o o array setTarefas linha 5
+    if(tarefaStorage){
+      //lembrando => como neste momento estamos trazendo lá dos Storage precisamos fazer a conversão para Parse
+      setTarefas(JSON.parse(tarefaStorage));
+    }
+  }, []);
+
+ //toda vez que o tarefa sofrer alteração => o useEffect vai ativar e salvar informação no locaStorage.
+  useEffect(()=> {
+    localStorage.setItem('tarefas', JSON.stringify(tarefas));
+  }, [tarefas])
+
+  const handleAdd = useCallback(()=>{
+    setTarefas([...tarefas, input])
+    setInput('');
+ }, [input, tarefas]);
+
+ //NESTE MOMENTO EU SUBSTITUIR handleAdd PELO => useCallback, DEVIDO A EXECUÇÃO SER MELHOR PARA PERFROMANCE DESTA APP
+  /*function handleAdd(){
+     setTarefas([...tarefas, input])
+     setInput('');
+  }*/
+ const totalTarefas = useMemo(()=> tarefas.length, [tarefas]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <ul>
+        {tarefas.map(tarefa => (
+           <li key={tarefa}>{tarefa}</li>
+        ))}
+      </ul> 
+      <br/>
+      <strong>Você tem {totalTarefas} tarefas!</strong><br/>
+      <input type="text" value={input} onChange={(e) => setInput(e.target.value)} />
+      <button type="button" onClick={handleAdd}>Adicionar</button>  
     </div>
   );
 }
